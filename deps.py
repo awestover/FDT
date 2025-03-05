fb2632c285306b198bd4585a0c957a0029e90270
Alek Westover <alek.westover@gmail.com>
2025-03-05 15:41:09 -0500

    update

--- deps.py
+++ deps.py
@@ -597,14 +597,14 @@ class DQN(nn.Module):
         return self.mlp(self.cnn_layers(x))
     
     def _initialize_weights(self):
-    for m in self.modules():
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            if m.bias is not None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
 
 
 # -----------------------
